b39ef960e29f8442a5b4fe50c2589edfece1b105
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-02-28 14:17:47 +0900

    Update train.py

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -97,7 +97,8 @@ for k, v in args.__dict__.items():
 logger("========================================")
 
 # seed
-args.cuda = torch.cuda.is_available(True)
+args.cuda = torch.cuda.is_available()
+printf(args.cuda)
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
