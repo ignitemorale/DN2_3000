e99eeeb8eff86a60ae3f730fc03ffc8076b0ef66
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-04-11 16:49:37 +0900

    Update train.py

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -256,6 +256,7 @@ try:
             logger('\tEpoch {} Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
                 epoch, test_loss, correct, len(test_loader.dataset), acc))
             accuracy = acc.cpu().data.numpy()
+            test_loss_1 = test_loss.cpu().data.numpy()
             np.savetxt(out, [[epoch, test_loss, accuracy]], delimiter=",",fmt='%f')
             
             if acc > best_acc:
